8956a7825b07a5f268237765bcdbad3bb2645d9e
Yosh Schoemaker <yosh.schoemaker@aceworks.nl>
2018-09-07 15:52:08 +0200

    Formatted data for feed & styling

--- my-app/src/js/components/watchlist-content.js
+++ my-app/src/js/components/watchlist-content.js
@@ -13,16 +13,58 @@ class watchlistContent extends PolymerElement{
             :host {
               display: block;
             }
+            p{
+              margin: 0;
+            }
+            .dateWritten{
+              margin-bottom: 2px;
+              font-size: 10pt;
+            }
+            .serieName{
+              color: #B71C1C;
+              font-weight: 600;
+            }
+            .row{
+              margin-top: 10px;
+              float: left;
+              width: 100%;
+              display: flex;
+              justify-content: space-between;
+            }
+            .row p{
+              float: left;
+              color: #3B3B3B;
+              margin-top: -1px;
+            }
+            .card{
+              float: left;
+              width: calc(100% - 72px);
+              margin-bottom: 10px;
+              margin-top: 10px;
+            }
+            .divider{
+              margin-top: 5px;
+              float: left;
+              width: 100%;
+              height: 1px;
+              background-color: #f2f2f2;
+            }
+            .icon{
+              width: 15px;
+              margin-right: 5px;
+              margin-top: 2px;
+              float: left;
+            }
+            .row .se{
+              color: #a2a2a2;
+            }
+            .icon-clock{
+              margin-top: 3px;
+            }
+            
+
         </style>
     `;
-  }
-
-  static get properties() {
-    return {
-      shows: {
-        type: Object
-      }
-    }
   }
 
   constructor() {
@@ -79,10 +121,45 @@ class watchlistContent extends PolymerElement{
     items.forEach(item => {
       var newItem = document.createElement('div');
       newItem.classList.add("card");
-      newItem.innerHTML = '<div>Title: ' + item.seriename + '</div>' +
-                          '<div>S' + item.seasonnumber + 'E' + item.episodenumber + '</div>';
+      newItem.innerHTML = '<p class="dateWritten">' + this.getDay(item.episodereleasedate) + '</p>' +
+                          '<p class="serieName">' + item.seriename + '<p>' +
+                          '<div class="divider"></div> ' +
+                          '<div class="row"><span><img src="images/icons/calendar.svg" alt="Calendar icon" class="icon"><p class="date">' + this.formatDate(item.episodereleasedate) + '</p></span>' +
+                          '<span><img src="images/icons/clock.svg" alt="Clock icon" class="icon icon-clock"><p>' + item.episodereleasetime + '</p></span>' +
+                          '<p class="se">S' + item.seasonnumber + 'E' + item.episodenumber + '</p></div>';
       this.shadowRoot.appendChild(newItem); // Add the new item to the shadowroot
     });
+  }
+
+  /* Find if the episode is aired today, tomorrow, has already been aired or will be */
+  getDay(date){
+
+    // Split date and convert to date object
+    var parts = date.split('-');
+    // Please pay attention to the month (parts[1]); JavaScript counts months from 0:
+    // January - 0, February - 1, etc.
+    var convertedDate = new Date(parts[0], parts[1] - 1, parts[2]); 
+
+    var date_today = new Date(); // Current date (today)
+
+    var date_tomorrow = new Date(date_today.getFullYear(), date_today.getMonth(), date_today.getDate() + 1); // Date of tomorrow
+
+    // Check the date
+    if (convertedDate.getFullYear() == date_today.getFullYear() && convertedDate.getMonth() == date_today.getMonth() && convertedDate.getDate() == date_today.getDate()){
+      return 'Today';
+    }else if(convertedDate.getFullYear() == date_tomorrow.getFullYear() && convertedDate.getMonth() == date_tomorrow.getMonth() && convertedDate.getDate() == date_tomorrow.getDate()){
+      return 'Tomorrow';
+    }else if (convertedDate < date_today){
+      return 'Aired'; // If show is already aired (input date < current date)
+    }else{
+      return 'Upcoming'; // Show is not today or tomorrow
+    }
+  }
+
+  /* Format the date into date/month format */
+  formatDate(date){
+    var parts = date.split('-');
+    return parts[2] + '/' + parts[1];
   }
 
   /* Create IndexedDB */
