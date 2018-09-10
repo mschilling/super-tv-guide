b76bda3bde4bc5cd0d11105decfcc1c7ba1009fc
RickTimmer <ricktimmer2@live.nl>
2018-09-10 09:10:39 +0100

    Added feed router with test data.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,18 +2,16 @@ import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as cors from 'cors';
 
-import { TVDBManager } from './TVDBManager';
+import { feed } from './testfeed'; // Test feed with test data
 
 const app = express();
 const router = express.Router();
 
-const tvdb = new TVDBManager();
-
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
 
-router.get('/ping', (req, res) => {
-  res.json({ result: 'pong', timestamp: new Date().toISOString() });
+router.get('/feed', (req, res) => {
+    res.json(feed);
 });
 
 app.use('/api', router);
---
+++ functions/src/testfeed.ts
@@ -0,0 +1,113 @@
+export const feed = 
+[
+	{
+		"serieid": 311900,
+		"seriename": "Shooter",
+		"serieimgurl": "graphical/311900-g3.jpg",
+		"seasonnumber": 3,
+		"episodenumber": 12,
+		"episodename": null,
+		"episodedescription": null,
+		"episodereleasedate": "2018-09-06",
+		"episodereleasetime": "10:00 PM"
+	},
+	{
+		"serieid": 104271,
+		"seriename": "Goede Tijden Slechte Tijden",
+		"serieimgurl": "graphical/104271-g4.jpg",
+		"seasonnumber": 41,
+		"episodenumber": 4,
+		"episodename": "Aflevering 5849",
+		"episodedescription": "Donderdag 6 september 2018\r\nLucas verzwijgt zijn diepste zorgen. Sjors heeft een nieuwe baan en stoot haar werkzaamheden voor Amirs stichting af. Anton vraagt zich af waarom Amir daarmee instemt. Aysen krijgt informatie over iemand, maar hoe betrouwbaar is de bron?",
+		"episodereleasedate": "2018-09-07",
+		"episodereleasetime": "8:00 PM"
+	},
+	{
+		"serieid": 104271,
+		"seriename": "Goede Tijden Slechte Tijden",
+		"serieimgurl": "graphical/104271-g4.jpg",
+		"seasonnumber": 41,
+		"episodenumber": 5,
+		"episodename": "Aflevering 5850",
+		"episodedescription": "Vrijdag 7 september 2018\r\nJanine kan het niet aan als de zaak rond Nina geen prioriteit meer heeft. Bing krijgt te maken met een oplichter. Als CS in de problemen komt, concludeert Kimberly dat zij daar verantwoordelijk voor is. Door Marieke verandert de situatie in CS zelfs in een regelrechte crisis.",
+		"episodereleasedate": "2018-09-08",
+		"episodereleasetime": "8:00 PM"
+	},
+	{
+		"serieid": 104271,
+		"seriename": "Goede Tijden Slechte Tijden",
+		"serieimgurl": "graphical/104271-g4.jpg",
+		"seasonnumber": 41,
+		"episodenumber": 6,
+		"episodename": "Aflevering 5851",
+		"episodedescription": null,
+		"episodereleasedate": "2018-09-10",
+		"episodereleasetime": "8:00 PM"
+	},
+	{
+		"serieid": 104271,
+		"seriename": "Goede Tijden Slechte Tijden",
+		"serieimgurl": "graphical/104271-g4.jpg",
+		"seasonnumber": 41,
+		"episodenumber": 7,
+		"episodename": "Aflevering 5852",
+		"episodedescription": null,
+		"episodereleasedate": "2018-09-11",
+		"episodereleasetime": "8:00 PM"
+	},
+	{
+		"serieid": 75805,
+		"seriename": "It's always sunny in philadelphia",
+		"serieimgurl": "graphical/5b48ed05cbe52.jpg",
+		"seasonnumber": 13,
+		"episodenumber": 2,
+		"episodename": null,
+		"episodedescription": null,
+		"episodereleasedate": "2018-09-12",
+		"episodereleasetime": "8:00 PM"
+	},
+	{
+		"serieid": 104271,
+		"seriename": "Goede Tijden Slechte Tijden",
+		"serieimgurl": "graphical/104271-g4.jpg",
+		"seasonnumber": 41,
+		"episodenumber": 8,
+		"episodename": "Aflevering 5853",
+		"episodedescription": null,
+		"episodereleasedate": "2018-09-12",
+		"episodereleasetime": "10:30 PM"
+	},
+	{
+		"serieid": 311900,
+		"seriename": "Shooter",
+		"serieimgurl": "graphical/311900-g3.jpg",
+		"seasonnumber": 3,
+		"episodenumber": 13,
+		"episodename": null,
+		"episodedescription": null,
+		"episodereleasedate": "2018-09-13",
+		"episodereleasetime": "4:00 PM"
+	},
+	{
+		"serieid": 104271,
+		"seriename": "Goede Tijden Slechte Tijden",
+		"serieimgurl": "graphical/104271-g4.jpg",
+		"seasonnumber": 41,
+		"episodenumber": 9,
+		"episodename": "Aflevering 5854",
+		"episodedescription": null,
+		"episodereleasedate": "2018-09-13",
+		"episodereleasetime": "8:00 PM"
+	},
+	{
+		"serieid": 104271,
+		"seriename": "Goede Tijden Slechte Tijden",
+		"serieimgurl": "graphical/104271-g4.jpg",
+		"seasonnumber": 41,
+		"episodenumber": 10,
+		"episodename": "Aflevering 5855",
+		"episodedescription": null,
+		"episodereleasedate": "2018-09-14",
+		"episodereleasetime": "8:00 PM"
+	}
+]
